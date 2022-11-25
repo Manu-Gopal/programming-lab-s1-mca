8ef15a8a7dbaed4e1e1b2c6fe80930c15f14dea3
Manu-Gopal <116630345+Manu-Gopal@users.noreply.github.com>
2022-11-25 11:33:55 +0530

    Delete cycle2_7_append_ing.py

--- cycle2_7_append_ing.py
+++
@@ -1,9 +0,0 @@
-def character_freq():
-    str=input("enter the string ")
-    length=len(str)
-    if "ing" in str:
-        str=str+"ly"
-    elif "ing" not in str:
-        str=str+"ing"
-    print(str)
-character_freq()
